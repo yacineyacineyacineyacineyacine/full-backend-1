fba2460835ef7910bed825324306c136f5876a3a
yacine <yacineyacine31212@gmail.com>
2025-04-23 19:04:27 +0100

    add endpoint for fetching data

--- server.js
+++ server.js
@@ -2,12 +2,21 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000
 
+let data = {
+    name: "Adel",
+}
+
 app.get("/", (req, res)=>{
-    res.send("hello world")
+    res.send("<h1>home page</h1>")
 });
 
 app.get("/dashboard", (req, res) =>{
-    res.status(200).send("hi")
+    res.status(200).send("<h1>dashbord</h1>")
+});
+
+app.get("/api/data", (req, res) =>{
+    res.status(200).send(data);
+
 })
 
 
