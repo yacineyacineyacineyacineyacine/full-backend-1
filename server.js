587ddedc5e8462e9123f0c858c9613258726ccf7
yacine <yacineyacine31212@gmail.com>
2025-04-23 18:21:32 +0100

    add command to run no dev mode

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 3000
 
 app.get("/", (req, res)=>{
     res.send("hello world")
 });
 
 
-app.listen(3000, ()=>{
-    console.log("server running");
+app.listen(PORT, ()=>{
+    console.log(`server running on ${PORT}`);
     
 })
